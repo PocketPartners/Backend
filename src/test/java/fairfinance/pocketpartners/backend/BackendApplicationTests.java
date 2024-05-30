ef5601def88d0aafe73666b64d0aee21d0514ec3
JCarbajal22 <u202121881@upc.edu.pe>
2024-05-30 13:21:43 -0500

    chore: initial commit

---
+++ src/test/java/fairfinance/pocketpartners/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fairfinance.pocketpartners.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
