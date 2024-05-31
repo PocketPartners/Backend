3b5ae18bfc379633548411f9b1790578d35359e8
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-31 00:14:34 -0500

    feat(group): added group entity and valueobject
    bug to fix reserved word “groups” you cannot create table with this name. thame called “pocket_groups” means while.

---
+++ src/main/java/fairfinance/pocketpartners/backend/groups/domain/model/aggregates/Group.java
@@ -0,0 +1,27 @@
+package fairfinance.pocketpartners.backend.groups.domain.model.aggregates;
+
+import fairfinance.pocketpartners.backend.groups.domain.model.valueobjects.GroupName;
+import fairfinance.pocketpartners.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "pocket_groups")
+public class Group extends AuditableAbstractAggregateRoot<Group> {
+
+    @Embedded
+    private GroupName name;
+
+    public Group(GroupName name) {
+        this.name = name;
+    }
+
+    public Group() {
+        this.name = new GroupName();
+    }
+
+    public void changeName(GroupName name) {
+        this.name = name;
+    }
+}
