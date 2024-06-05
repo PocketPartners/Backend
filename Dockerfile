ec483d9790b5fbd29d8c665c048fa3b3509aadf9
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-06-05 11:04:36 -0500

    fix(cors): added the url to match corns config
    and added the docker config deleted

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
-#
-# Build stage
-#
-FROM maven:3.9.7 AS build
-VOLUME /tmp
-COPY . .
-RUN mvn clean package
-
-FROM openjdk:22-jdk AS runtime
+FROM openjdk:22-jdk
 VOLUME /tmp
 EXPOSE 8080
-COPY fairfinance-pocketpartners-backend-0.0.1-SNAPSHOT.jar app.jar
+COPY target/*.jar app.jar
 ENTRYPOINT java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
--- src/main/java/fairfinance/pocketpartners/backend/BackendApplication.java
+++ src/main/java/fairfinance/pocketpartners/backend/BackendApplication.java
@@ -3,11 +3,11 @@ package fairfinance.pocketpartners.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
