10c0d344c411e9a4ca63621e221c851b0c201f34
JCarbajal22 <u202121881@upc.edu.pe>
2024-06-18 11:40:54 -0500

    fix(secutiry): working.

---
+++ src/main/java/fairfinance/pocketpartners/backend/config/WebConfig.java
@@ -0,0 +1,31 @@
+package fairfinance.pocketpartners.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://poketpartners-deployment.vercel.app")
+                        .allowedOrigins("https://fair-finance-poketpartners.web.app")
+                        .allowedOrigins("https://pocket-partners-frontend.vercel.app")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION);
+            }
+        };
+    }
+}
